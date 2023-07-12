c257bca2212498828968402cb505b5a9d21a464e
JaeHo Song <thruthesky@gmail.com>
2023-07-12 17:04:06 +0800

    adding chat room list & chat freind list

--- lib/easychat.dart
+++ lib/easychat.dart
@@ -1,7 +1,2 @@
-library easychat;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/chat_friend_list.dart';
+export 'src/chat_room_list.dart';
