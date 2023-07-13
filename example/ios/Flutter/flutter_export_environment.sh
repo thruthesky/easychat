1e86be4aa83139463bca6506d671966a59399725
JaeHo Song <thruthesky@gmail.com>
2023-07-13 14:16:31 +0800

    removing anroid

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/thruthesky/bin/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/thruthesky/apps/flutter/sonub_flutter/lib/packages/easychat/example"
+export "FLUTTER_APPLICATION_PATH=/Users/thruthesky/apps/flutter/packages/easychat/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
