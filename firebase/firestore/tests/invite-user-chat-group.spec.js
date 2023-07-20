a05d63785db52556c948213a3a4faa0535d4bd4a
Christian Bucal <withcenter-dev2@github.com>
2023-07-20 17:30:35 +0800

    progress in invite

--- firebase/firestore/tests/invite-user-chat-group.spec.js
+++ firebase/firestore/tests/invite-user-chat-group.spec.js
@@ -4,13 +4,13 @@ const { db, a, b, c, createChatRoom } = require("./setup");
 // load firebase-functions-test SDK
 const firebase = require("@firebase/testing");
 
-describe("Firestore security test chat room", () => {
-    it("User A inviting user B to an open room made by user A -> success", async () => {
+describe("Firestore security test adding a user in chat room", () => {
+    it("User A inviting user B to an open room made by user A -> succeed", async () => {
         // A creating open room
         const roomRef = await createChatRoom(a, { master: a.uid, users: [a.uid], open: true, group: true });
         // A inviting B
         await firebase.assertSucceeds(
-            await db(a)
+            db(a)
                 .collection("easychat")
                 .doc(roomRef.id)
                 .update({ users: firebase.firestore.FieldValue.arrayUnion(b.uid) })
@@ -21,12 +21,13 @@ describe("Firestore security test chat room", () => {
             .get();
         assert.ok(doc.data().users.includes(b.uid));
     });
-    it("User A inviting user B to an open room made by user A -> success", async () => {
+
+    it("User A inviting user B to an open room made by user A -> succeed", async () => {
         // A creating open room
         const roomRef = await createChatRoom(a, { master: a.uid, users: [a.uid], open: false, group: true });
         // A inviting B
         await firebase.assertSucceeds(
-            await db(a)
+            db(a)
                 .collection("easychat")
                 .doc(roomRef.id)
                 .update({ users: firebase.firestore.FieldValue.arrayUnion(b.uid) })
@@ -38,8 +39,9 @@ describe("Firestore security test chat room", () => {
         // Master can still invite a user even if the Group is closed
         assert.ok(doc.data().users.includes(b.uid));
     });
-    it("User B inviting user C to an open room made by user A -> failure", async () => {
-        // A creating closed room
+
+    it("User B inviting user C to an open room made by user A -> succeed", async () => {
+        // A creating open room
         const roomRef = await createChatRoom(a, { master: a.uid, users: [a.uid], open: true, group: true });
         // A inviting B
         await firebase.assertSucceeds(
@@ -54,60 +56,100 @@ describe("Firestore security test chat room", () => {
             .get();
         assert.ok(doc1.data().users.includes(b.uid));
         // B inviting C
-        await firebase.assertFails(
-            await db(b)
+        await firebase.assertSucceeds(
+            db(b)
                 .collection("easychat")
                 .doc(roomRef.id)
                 .update({ users: firebase.firestore.FieldValue.arrayUnion(c.uid) })
         );
-        const doc2 = await db()
+        const doc2 = await db(b)
             .collection("easychat")
             .doc(roomRef.id)
             .get();
-        // User C cannot be invited by B because B is not a master or moderator, and GC is closed.
-        assert.ok(!doc2.data().users.includes(c.uid));
+        // User C can be invited by B because GC is open.
+        assert.ok(doc2.data().users.includes(c.uid));
     });
-    // it("Inviting user C to the room by user B in an open room -> success", async () => {
-    //     // A creating room
-    //     const roomRef = await createChatRoom(a, { master: a.uid, users: [a.uid], open: true, group: true });
-    //     // A inviting B
-    //     await firebase.assertSucceeds(
-    //         await db(a)
-    //             .collection("easychat")
-    //             .doc(roomRef.id)
-    //             .update({ users: firebase.firestore.FieldValue.arrayUnion(b.uid) })
-    //     );
-    //     const doc1 = await db(a)
-    //         .collection("easychat")
-    //         .doc(roomRef.id)
-    //         .get();
-    //     assert.ok(doc1.data().users.includes(b.uid));
-    //     // B inviting C
-    //     await firebase.assertSucceeds(
-    //         await db(b)
-    //             .collection("easychat")
-    //             .doc(roomRef.id)
-    //             .update({ users: firebase.firestore.FieldValue.arrayUnion(c.uid) })
-    //     );
-    //     const doc2 = await db(b)
-    //         .collection("easychat")
-    //         .doc(roomRef.id)
-    //         .get();
-    //     assert.ok(doc2.data().users.includes(c.uid));
-    // });
-    // it("Inviting user C to the room by user B in an closed room -> success", async () => {
-    //     const roomRef = await createChatRoom(a, { master: a.uid, users: [a.uid, b.uid], open: true, group: true });
-    //     await firebase.assertSucceeds(
-    //         await db(b)
-    //             .collection("easychat")
-    //             .doc(roomRef.id)
-    //             .update({ users: firebase.firestore.FieldValue.arrayUnion(c.uid) })
-    //     );
-    //     const doc = await db(c)
-    //         .collection("easychat")
-    //         .doc(roomRef.id)
-    //         .get();
-    //     assert.ok(doc.data().users.includes(c.uid));
-    // });
+
+    it("User B inviting user C to an open room where B does not belong in this room -> failure", async () => {
+        // A creating open room
+        const roomRef = await createChatRoom(a, { master: a.uid, users: [a.uid], open: true, group: true });
+        // B inviting C to an open room created by A
+        await firebase.assertFails(
+            db(b)
+                .collection("easychat")
+                .doc(roomRef.id)
+                .update({ users: firebase.firestore.FieldValue.arrayUnion(c.uid) })
+        );
+        const doc1 = await db(a)
+            .collection("easychat")
+            .doc(roomRef.id)
+            .get();
+        assert.ok(!doc1.data().users.includes(c.uid));
+    });
+
+    it("User A inviting user B to an closed room by user A -> success", async () => {
+        // A creating open room
+        const roomRef = await createChatRoom(a, { master: a.uid, users: [a.uid], open: false, group: true });
+        // B inviting C to an open room created by A
+        await firebase.assertSucceeds(
+            db(a)
+                .collection("easychat")
+                .doc(roomRef.id)
+                .update({ users: firebase.firestore.FieldValue.arrayUnion(b.uid) })
+        );
+        const doc1 = await db(a)
+            .collection("easychat")
+            .doc(roomRef.id)
+            .get();
+        // Since A the master, A can invite anyone
+        assert.ok(doc1.data().users.includes(b.uid));
+    });
+
+    it("User B inviting user C to a 1:1 room -> failure", async () => {
+        // A creating closed room
+        const roomRef = await createChatRoom(a, { master: a.uid, users: [a.uid, b.uid], group: false });
+        // B inviting C
+        await firebase.assertFails(
+            db(b)
+                .collection("easychat")
+                .doc(roomRef.id)
+                .update({ users: firebase.firestore.FieldValue.arrayUnion(c.uid) })
+        );
+        const doc1 = await db(a)
+            .collection("easychat")
+            .doc(roomRef.id)
+            .get();
+        assert.ok(!doc1.data().users.includes(c.uid));
+    });
+
+    it("User B inviting user C to an close room -> failure", async () => {
+        // A creating closed room
+        const roomRef = await createChatRoom(a, { master: a.uid, users: [a.uid], open: false, group: true });
+        // A inviting B
+        await firebase.assertSucceeds(
+            db(a)
+                .collection("easychat")
+                .doc(roomRef.id)
+                .update({ users: firebase.firestore.FieldValue.arrayUnion(b.uid) })
+        );
+        const doc1 = await db(a)
+            .collection("easychat")
+            .doc(roomRef.id)
+            .get();
+        assert.ok(doc1.data().users.includes(b.uid));
+        // B inviting C
+        await firebase.assertFails(
+            db(b)
+                .collection("easychat")
+                .doc(roomRef.id)
+                .update({ users: firebase.firestore.FieldValue.arrayUnion(c.uid) })
+        );
+        const doc2 = await db(a)
+            .collection("easychat")
+            .doc(roomRef.id)
+            .get();
+        // C should not be here because B can't invite because GC is closed
+        assert.ok(!doc1.data().users.includes(c.uid));
+    });
 });
 
