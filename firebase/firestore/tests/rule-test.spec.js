74e5826861ec9a141d4b561c38043e2589f76342
JaeHo Song <thruthesky@gmail.com>
2023-07-21 17:53:09 +0800

    updating rules

--- firebase/firestore/tests/rule-test.spec.js
+++ firebase/firestore/tests/rule-test.spec.js
@@ -6,39 +6,86 @@ const firebase = require("@firebase/testing");
 
 describe("Rule tests", () => {
     it("onlyUpdating", async () => {
-
         const ref = await admin()
-            .collection("test")
+            .collection("rule-test-onlyUpdating")
             .add({ a: 1, b: 2, c: 3 });
         
         snapshot = await ref.get();
 
+
+        // 아무것도 업데이트 안해도 성공
         await firebase.assertSucceeds(
-            db(a).collection("test").doc(ref.id).update({
+            db(a).collection("rule-test-onlyUpdating").doc(ref.id).update({})
+        );
+        await firebase.assertSucceeds(
+            db(a).collection("rule-test-onlyUpdating").doc(ref.id).update({
                 a: 10,
             })
         );
         await firebase.assertSucceeds(
-            db(a).collection("test").doc(ref.id).update({
+            db(a).collection("rule-test-onlyUpdating").doc(ref.id).update({
                 b: 20,
             })
         );
         await firebase.assertSucceeds(
-            db(a).collection("test").doc(ref.id).update({
+            db(a).collection("rule-test-onlyUpdating").doc(ref.id).update({
                 a: 11, b: 22,
             })
         );
         await firebase.assertFails(
-            db(a).collection("test").doc(ref.id).update({
+            db(a).collection("rule-test-onlyUpdating").doc(ref.id).update({
                 a: 10, b: 20, c: 30
             })
         );
         await firebase.assertFails(
-            db(a).collection("test").doc(ref.id).update({
+            db(a).collection("rule-test-onlyUpdating").doc(ref.id).update({
+                c: 33
+            })
+        );
+    });
+    it("notUpdating", async () => {
+        const ref = await admin()
+            .collection("rule-test-notUpdating")
+            .add({ a: 1, b: 2, c: 3 });
+        
+        snapshot = await ref.get();
+
+
+
+        await firebase.assertSucceeds(
+            db(a).collection("rule-test-notUpdating").doc(ref.id).update({
+            })
+        );
+        await firebase.assertSucceeds(
+            db(a).collection("rule-test-notUpdating").doc(ref.id).update({
                 c: 33
             })
         );
 
+        await firebase.assertFails(
+            db(a).collection("rule-test-onlyUpdating").doc(ref.id).update({
+                a: 10,
+            })
+        );
+        await firebase.assertFails(
+            db(a).collection("rule-test-onlyUpdating").doc(ref.id).update({
+                b: 20,
+            })
+        );
+        await firebase.assertFails(
+            db(a).collection("rule-test-onlyUpdating").doc(ref.id).update({
+                a: 11, b: 22,
+            })
+        );
+        await firebase.assertFails(
+            db(a).collection("rule-test-onlyUpdating").doc(ref.id).update({
+                a: 10, b: 20, c: 30
+            })
+        );
     });
+
+    
+
+    
 });
 
