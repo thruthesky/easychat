4af44cf6d453844e0fcd66b56bcd48d113875227
JaeHo Song <thruthesky@gmail.com>
2023-07-22 11:29:36 +0800

    testing for pr

--- firebase/firestore/tests/rule-test.spec.js
+++ firebase/firestore/tests/rule-test.spec.js
@@ -2,6 +2,7 @@ const assert = require("assert");
 const { db, a, b, c, d, tempChatRoomData, admin } = require("./setup");
 
 // load firebase-functions-test SDK
+// .
 const firebase = require("@firebase/testing");
 
 describe("Rule tests", () => {
