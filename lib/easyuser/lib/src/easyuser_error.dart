ee02bc3f809fa9582a8f2a6fcf84ee948fa73c4c
JaeHo Song <thruthesky@gmail.com>
2023-08-14 14:49:25 +0800

    ref

--- lib/easyuser/lib/src/easyuser_error.dart
+++
@@ -1,5 +0,0 @@
-class EasyUserError {
-  static const notLoggedIn = 'User is not logged in.';
-  static const documentNotExist = 'User document does not exist.';
-  static const documentAlreadyExists = 'User document already exists.';
-}
